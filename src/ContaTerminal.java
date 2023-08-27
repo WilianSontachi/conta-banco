7558835d3024afe76cd75ab9a3d297b114f14534
WilianSontachi <wilianbatata29@gmail.com>
2023-08-27 02:32:18 -0300

    meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-
         Scanner scanner = new Scanner(System.in);
         
         int numero;
@@ -13,8 +12,14 @@ public class ContaTerminal {
         System.out.println("Por favor, digite seu nome: ");
         nomeCliente = scanner.nextLine();
 
-        System.out.println("Por favor, digite o número da agência: ");
+        System.out.println("Por favor, digite o número da agência com hífen: ");
         agencia = scanner.nextLine();
+
+        String[] partesAgencia = agencia.split("-");
+        
+        String numeroAgencia = partesAgencia[0].trim();
+        
+        String digitoAgencia = partesAgencia[1].trim();
 
         System.out.println("Por favor, digite o número da conta: ");
         numero = scanner.nextInt();
@@ -23,7 +28,7 @@ public class ContaTerminal {
         saldo = scanner.nextDouble();
 
         System.out.println();
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$"+ saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + "-" + digitoAgencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque.");
 
         scanner.close();
     }
